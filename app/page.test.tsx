3385efb514112a58d3c69dc46114498a13720031
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-11 20:10:19 +0900

    🎨style: add carousel

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,27 +1,4 @@
-import { expect, test } from "bun:test"
 import { render } from "@testing-library/react"
 import Home from "./page"
 
 render(<Home />)
-
-test("p test", () => {
-  const p = document.querySelectorAll("p")
-  expect(p[0]?.innerText).toEqual(
-    "Find in-depth information about Next.js features and API.",
-  )
-  expect(p[1]?.innerText).toEqual(
-    "Learn about Next.js in an interactive course with\u00A0quizzes!",
-  )
-  expect(p[2]?.innerText).toEqual("Explore starter templates for Next.js.")
-  expect(p[3]?.innerText).toEqual(
-    "Instantly deploy your Next.js site to a shareable URL with Vercel.",
-  )
-})
-
-test("h2 test", () => {
-  const h2 = document.querySelectorAll("h2")
-  expect(h2[0]?.innerText).toEqual("Docs ->")
-  expect(h2[1]?.innerText).toEqual("Learn ->")
-  expect(h2[2]?.innerText).toEqual("Templates ->")
-  expect(h2[3]?.innerText).toEqual("Deploy ->")
-})
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "caravan-kidstec.s3.ap-northeast-1.amazonaws.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 }
 
 export default nextConfig

40b46626f939c0702dc9ccdda6ca92efd45dadfc
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-12 18:02:08 +0900

    🎨style: change font weight

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -13,7 +13,7 @@ export function Footer(): React.JSX.Element {
               {item.href && (
                 <Link
                   href={item.href as string}
-                  className="break-keep link-hover"
+                  className="break-keep font-bold link-hover"
                 >
                   <item.icon className={`size-5 ${item.color}`} />
                   {item.name}
@@ -21,7 +21,7 @@ export function Footer(): React.JSX.Element {
               )}
               {item.content && (
                 <>
-                  <span className="break-keep">
+                  <span className="break-keep font-bold">
                     <item.icon className={`size-5 ${item.color}`} />
                     {item.name}
                   </span>

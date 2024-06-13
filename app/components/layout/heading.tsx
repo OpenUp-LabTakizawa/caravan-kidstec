281fe4aa041c9c12d5187b4bc2571bb0438ef309
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-13 16:18:07 +0900

    ✨feat: add default pages

--- app/components/layout/heading.tsx
+++ app/components/layout/heading.tsx
@@ -5,18 +5,20 @@ import { navigation } from "@/app/lib/constant"
 import type React from "react"
 
 export function Heading({
+  content,
   href,
-  name,
-}: { href: string; name: string }): React.JSX.Element {
-  const crumb: Navigation = navigation.filter((item) => item.name === name)[0]
-  const content: SiteUrl = crumb.content?.filter(
+}: { content: string; href: string }): React.JSX.Element {
+  const crumb: Navigation = navigation.filter(
+    (item) => item.name === content,
+  )[0]
+  const heading: SiteUrl = crumb.content?.filter(
     (item) => item.href === href,
   )[0] as SiteUrl
 
   return (
     <section>
       <div className="max-w-fit mx-auto">
-        <h1 className="typing font-bold text-3xl">{content.name}</h1>
+        <h1 className="typing font-bold text-3xl">{heading.name}</h1>
       </div>
       <Breadcrumb crumb={crumb} href={href} />
     </section>
---
+++ app/supporter/page.tsx
@@ -0,0 +1,6 @@
+import { Heading } from "@/app/components/layout/heading"
+import type React from "react"
+
+export default function Supporter(): React.JSX.Element {
+  return <Heading content="サポーター/パートナー" href="/supporter" />
+}

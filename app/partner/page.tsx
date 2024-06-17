fc3f9798fbcf84b7915d7dd2d66fb9742c69e7eb
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-17 18:35:02 +0900

    🎨style: 2 grid columns on partner page

--- app/partner/page.tsx
+++ app/partner/page.tsx
@@ -7,9 +7,11 @@ export default function Partner(): React.JSX.Element {
   return (
     <article className="grid gap-6 sm:px-12">
       <Heading content="サポーター/パートナー" href="/partner" />
-      <section className="grid gap-6 p-4">
-        <h2 className="font-bold text-2xl text-center">広島エリア</h2>
-        <section className="bg-base-200 grid gap-4 p-4">
+      <section className="grid gap-6 p-4 sm:grid-cols-2">
+        <h2 className="font-bold text-2xl text-center sm:col-span-2">
+          広島エリア
+        </h2>
+        <section className="bg-base-200 grid gap-4 p-4 w-fit mx-auto">
           <h3 className="font-bold text-center text-xl">
             <Link href="https://bplab.biz/" target="_blank" className="link">
               BPL Inc.
@@ -20,10 +22,10 @@ export default function Partner(): React.JSX.Element {
             width={1000}
             height={1000}
             alt="bpl_logo"
-            className="w-full"
+            className="w-full sm:max-h-60 sm:object-contain"
           />
         </section>
-        <section className="bg-base-200 grid gap-4 p-4">
+        <section className="bg-base-200 grid gap-4 p-4 w-fit mx-auto">
           <h3 className="font-bold text-center text-xl">
             <Link href="https://fuudo.jp/" target="_blank" className="link">
               一般社団法人 フウド
@@ -34,10 +36,10 @@ export default function Partner(): React.JSX.Element {
             width={1000}
             height={1000}
             alt="fuudo_logo"
-            className="w-full"
+            className="w-full sm:max-h-60 sm:object-contain"
           />
         </section>
-        <section className="bg-base-200 grid gap-4 p-4">
+        <section className="bg-base-200 grid gap-4 p-4 w-fit mx-auto">
           <h3 className="font-bold text-center text-xl">
             <Link
               href="https://sanken-hiroshima.org/"
@@ -54,17 +56,17 @@ export default function Partner(): React.JSX.Element {
             width={1000}
             height={1000}
             alt="sanken_logo"
-            className="w-full"
+            className="w-full sm:max-h-60 sm:object-contain"
           />
         </section>
-        <section className="bg-base-200 grid gap-4 p-4">
+        <section className="bg-base-200 grid gap-4 p-4 w-fit mx-auto">
           <h3 className="font-bold text-center text-xl">広島大学 さんけん部</h3>
           <Image
             src="https://caravan-kidstec.s3.ap-northeast-1.amazonaws.com/logo/sanken_club.webp"
             width={1000}
             height={1000}
             alt="sanken_club"
-            className="w-full"
+            className="w-full sm:max-h-60 sm:object-contain"
           />
         </section>
       </section>

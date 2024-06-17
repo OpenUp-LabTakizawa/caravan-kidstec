4e3b4f8c9035a82f8c141ed0405dc8e36ab40829
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-17 16:30:58 +0900

    🚑fix: title length

--- app/partner/page.tsx
+++ app/partner/page.tsx
@@ -2,5 +2,12 @@ import { Heading } from "@/app/components/layout/heading"
 import type React from "react"
 
 export default function Partner(): React.JSX.Element {
-  return <Heading content="サポーター/パートナー" href="/partner" />
+  return (
+    <article className="grid gap-6 sm:px-12">
+      <Heading content="サポーター/パートナー" href="/partner" />
+      <section className="bg-base-200 grid gap-6 p-4">
+        <h2 className="font-bold text-2xl text-center">広島エリア</h2>
+      </section>
+    </article>
+  )
 }

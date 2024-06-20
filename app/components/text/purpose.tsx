c3e93cd56ebf2d87a6c19bbf6007d806345937f5
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-20 17:00:07 +0900

    🎨style: define handshake section

--- app/components/text/purpose.tsx
+++ app/components/text/purpose.tsx
@@ -1,115 +1,8 @@
 import type React from "react"
-import { Divider } from "../layout/divider"
 
 export function Purpose(): React.JSX.Element {
   return (
     <>
-      <h3>先程のパターンA（文字サイズ小、太さ普通）</h3>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="hero-content grid text-center">
-          <p className="">
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      <Divider />
-      <h3>先程のパターンC（文字サイズ小、太さ大）</h3>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="font-bold hero-content grid text-center">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      <Divider />
-      <h3>グレーパターンA：rgb(107 114 128)</h3>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="hero-content grid text-center text-gray-500">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      <Divider />
-      <h3>グレーパターンC：rgb(107 114 128)</h3>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="font-bold hero-content grid text-center text-gray-500">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      <Divider />
-      <h3>グレー2パターンA：rgb(55 65 81)</h3>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="hero-content grid text-center text-gray-700">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      <Divider />
-      <h3>グレー2パターンC：rgb(55 65 81)</h3>
       <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
         <div className="hero-overlay bg-base-100 bg-opacity-80" />
         <div className="font-bold hero-content grid text-center text-gray-700">
@@ -129,9 +22,7 @@ export function Purpose(): React.JSX.Element {
           </p>
         </div>
       </section>
-      {/* <Divider />
-      <h3>パターンA</h3>
-      <section className="text-xs">
+      {/* <section className="text-xs">
         <Image
           src="/handshake.webp"
           alt="handshake"

45b4408555339832891d53612f20f23cd4bc4663
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-26 16:37:30 +0900

    🚑fix: square top carousel

--- app/area/chiba/page.tsx
+++ app/area/chiba/page.tsx
@@ -1,11 +1,65 @@
 import { Heading } from "@/app/components/layout/heading"
 import { Video } from "@/app/components/media/video"
+import { ChibaEvents } from "@/app/lib/constant"
+import { ArrowRightIcon } from "@heroicons/react/24/solid"
+import Image from "next/image"
+import Link from "next/link"
 import type React from "react"
 
 export default function Chiba(): React.JSX.Element {
   return (
     <>
       <Heading content="開催予定" href="/area/chiba" />
+      <section id="news" className="grid gap-4">
+        <Image
+          src={"/temp_event_thumbnail.webp"}
+          width={1000}
+          height={1000}
+          alt="こどもテックキャラバン"
+          className="w-full"
+        />
+        <div className="bg-amber-50 grid mx-auto w-max">
+          <span className="bg-sky-400 font-bold justify-self-start p-1">
+            開催日時
+          </span>
+          <ul>
+            {ChibaEvents.map((item) => (
+              <li key={item.venue.name} className="grid gap-1 border-b p-2">
+                <p className="decoration-4 decoration-sky-400 underline">
+                  2024年
+                  <span className="font-bold text-2xl">{item.date.month}</span>
+                  月<span className="font-bold text-2xl">{item.date.day}</span>
+                  日{"("}
+                  <span className="font-bold text-xl">{item.date.weekday}</span>
+                  {")"}
+                </p>
+                <div className="flex gap-2 items-center mx-auto">
+                  <p className="badge badge-outline">場所</p>
+                  <p>
+                    <strong>{item.venue.name}</strong>
+                    <br />
+                    {item.venue.address}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="bg-amber-50 grid gap-1 mx-auto p-4 w-max">
+          <p>
+            <Link href="/area/chiba" className="link link-info">
+              応募要項
+            </Link>
+            をご確認の上、
+            <br />
+            応募画面へお進み下さい。
+          </p>
+          <button type="button" className="bg-[#00C300] btn text-lg text-white">
+            LINEで簡単応募する！
+            <ArrowRightIcon className="size-5" />
+          </button>
+        </div>
+      </section>
       <Video src="https://caravan-kidstec.s3.ap-northeast-1.amazonaws.com/movie/202312" />
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import Link from "next/link"
 import type React from "react"
 import { Banner } from "./components/layout/banner"
 import { Video } from "./components/media/video"
-import { ChibaEvents } from "./lib/constant"
 
 export default function Home(): React.JSX.Element {
   return (
@@ -73,55 +72,19 @@ export default function Home(): React.JSX.Element {
           </p>
         </div>
       </section>
-      <section id="news" className="grid gap-4">
-        <Image
-          src={"/temp_event_thumbnail.webp"}
-          width={1000}
-          height={1000}
-          alt="こどもテックキャラバン"
-          className="w-full"
-        />
-        <div className="bg-amber-50 grid mx-auto w-max">
-          <span className="bg-sky-400 font-bold justify-self-start p-1">
-            開催日時
-          </span>
-          <ul>
-            {ChibaEvents.map((item) => (
-              <li key={item.venue.name} className="grid gap-1 border-b p-2">
-                <p className="decoration-4 decoration-sky-400 underline">
-                  2024年
-                  <span className="font-bold text-2xl">{item.date.month}</span>
-                  月<span className="font-bold text-2xl">{item.date.day}</span>
-                  日{"("}
-                  <span className="font-bold text-xl">{item.date.weekday}</span>
-                  {")"}
-                </p>
-                <div className="flex gap-2 items-center mx-auto">
-                  <p className="badge badge-outline">場所</p>
-                  <p>
-                    <strong>{item.venue.name}</strong>
-                    <br />
-                    {item.venue.address}
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="bg-amber-50 grid gap-1 mx-auto p-4 w-max">
-          <p>
-            <Link href="/area/chiba" className="link link-info">
-              応募要項
-            </Link>
-            をご確認の上、
-            <br />
-            応募画面へお進み下さい。
-          </p>
-          <button type="button" className="bg-[#00C300] btn text-lg text-white">
-            LINEで簡単応募する！
-            <ArrowRightIcon className="size-5" />
-          </button>
-        </div>
+      <section className="bg-amber-50 grid gap-1 mx-auto p-4 w-max">
+        <p>
+          <Link href="/area/chiba" className="link link-info">
+            応募要項
+          </Link>
+          をご確認の上、
+          <br />
+          応募画面へお進み下さい。
+        </p>
+        <button type="button" className="bg-[#00C300] btn text-lg text-white">
+          LINEで簡単応募する！
+          <ArrowRightIcon className="size-5" />
+        </button>
       </section>
       <section className="grid gap-4">
         <h2 className="font-bold font-zenMaruGothic text-3xl text-orange-400">

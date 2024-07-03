184f371e79da09caa96422cd285ee946f51d384e
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-07-03 11:57:42 +0900

    🚑fix: optimize image

--- app/components/text/purpose.tsx
+++ app/components/text/purpose.tsx
@@ -1,52 +1,32 @@
+import Image from "next/image"
 import type React from "react"
 
 export function Purpose(): React.JSX.Element {
   return (
-    <>
-      <section className="hero bg-[url('/handshake.webp')] bg-contain bg-no-repeat text-sm">
-        <div className="hero-overlay bg-base-100 bg-opacity-80" />
-        <div className="font-bold hero-content grid text-center text-gray-700">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section>
-      {/* <section className="text-xs">
-        <Image
-          src="/handshake.webp"
-          alt="handshake"
-          width={1000}
-          height={1000}
-          className="w-full object-contatin opacity-30"
-        />
-        <div className="grid text-center text-green-600 z-10">
-          <p>
-            こどもテックキャラバンでは、
-            <br />
-            様々な企業や団体が手を取り合い、
-            <br />
-            社会課題解決を 目指しています。
-          </p>
-          <p>
-            わたしたちは 関わる全ての人が、
-            <br />
-            学び、遊び、心 沸き立つような可能性が
-            <br />
-            広がる社会を期待しています。
-          </p>
-        </div>
-      </section> */}
-    </>
+    <section className="hero text-sm max-h-min">
+      <Image
+        src="/handshake.webp"
+        alt="handshake"
+        width={1000}
+        height={1000}
+        className="max-h-48 object-contain opacity-20 -z-10"
+      />
+      <div className="font-bold hero-content grid text-center text-gray-700">
+        <p>
+          こどもテックキャラバンでは、
+          <br />
+          様々な企業や団体が手を取り合い、
+          <br />
+          社会課題解決を 目指しています。
+        </p>
+        <p>
+          わたしたちは 関わる全ての人が、
+          <br />
+          学び、遊び、心 沸き立つような可能性が
+          <br />
+          広がる社会を期待しています。
+        </p>
+      </div>
+    </section>
   )
 }

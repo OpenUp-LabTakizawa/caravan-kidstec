345939a2f6303aa30cb7236cac9d3a2a4a71ae14
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-08-30 15:25:01 +0900

    🎨style: add margin to sponser logo (#842)

--- app/sponser/page.tsx
+++ app/sponser/page.tsx
@@ -35,7 +35,7 @@ export default function SponserPage(): JSX.Element {
             key={sponser.name}
             className="bg-base-200 gap-1 grid grid-cols-6 items-center min-h-32 p-1 rounded-2xl shadow-lg sm:grid-cols-2"
           >
-            <div className="bg-white col-span-2 flex h-full items-center rounded-2xl my-4 sm:col-span-1">
+            <div className="bg-white col-span-2 flex h-full items-center rounded-2xl px-2 sm:px-0 sm:col-span-1">
               <Image
                 loader={cloudfrontLoader}
                 src={sponser.src}

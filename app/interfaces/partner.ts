d2c92698c978f1df17097636e84393d1b212e63b
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-09-17 13:19:51 +0900

    ✨feat: add partner page (#934)
    * ✨feat: add partner page
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- app/interfaces/partner.ts
+++ app/interfaces/partner.ts
@@ -1,6 +1,17 @@
+import type { Submenu } from "@/app/interfaces/menu"
+
 export type Partner = {
   readonly name: string
   readonly href: string
   readonly src: string
   readonly introduction: string
+}
+
+export type PartnerArea = {
+  readonly submenu: Submenu
+  readonly partners: Partner[]
+  readonly color: {
+    border: string
+    text: string
+  }
 }

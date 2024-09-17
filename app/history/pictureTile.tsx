e00d041d517bfdfafa944e52c8bf4838b4c9a980
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-09-17 15:47:55 +0900

    ✨feat: readd picture tile

---
+++ app/history/pictureTile.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import type { TilePicture } from "@/app/interfaces/picture"
+import { HIROSHIMA, HISTORY } from "@/app/lib/constant"
+import { cloudfrontLoader } from "@/app/lib/loader"
+import { MagnifyingGlassPlusIcon } from "@heroicons/react/24/solid"
+import Image from "next/image"
+import Link from "next/link"
+import { type JSX, type RefObject, useEffect, useRef } from "react"
+
+export function PictureTile({
+  date,
+  pictures,
+}: Readonly<{ date: string; pictures: TilePicture[] }>): JSX.Element {
+  const ref: RefObject<Map<string, HTMLAnchorElement>> = useRef<
+    Map<string, HTMLAnchorElement>
+  >(new Map<string, HTMLAnchorElement>())
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("scale-up-down")
+        } else {
+          entry.target.classList.remove("scale-up-down")
+        }
+      }
+    })
+
+    if (ref.current?.values()) {
+      for (const node of ref.current.values()) {
+        observer.observe(node)
+      }
+    }
+    return () => observer.disconnect()
+  })
+
+  return (
+    <div className="gap-1 grid grid-cols-3">
+      {pictures.map((picture) => (
+        <Link
+          key={picture.alt}
+          href={`${HISTORY.pathname + HIROSHIMA.pathname}/image/${date}/${picture.name}`}
+          ref={(node: HTMLAnchorElement) => {
+            ref.current?.set(picture.alt, node)
+            return () => {
+              ref.current?.delete(picture.alt)
+            }
+          }}
+          className="relative"
+        >
+          <MagnifyingGlassPlusIcon className="absolute right-0 size-4 stroke-2 text-white top-0" />
+          <Image
+            loader={cloudfrontLoader}
+            src={picture.src}
+            width={256}
+            height={256}
+            alt={picture.alt}
+            className="w-full"
+          />
+        </Link>
+      ))}
+    </div>
+  )
+}

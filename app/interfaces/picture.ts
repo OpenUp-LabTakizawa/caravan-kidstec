5d7a5340a50db1942acbc646a7b8605658af5214
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-09-19 12:15:55 +0900

    🔥delete: unused types (#956)

--- app/interfaces/picture.ts
+++ app/interfaces/picture.ts
@@ -2,21 +2,3 @@ export type Picture = {
   readonly alt: string
   readonly src: string
 }
-
-export type TilePicture = {
-  readonly alt: string
-  readonly name: string
-  readonly src: string
-}
-
-export type Panel = {
-  readonly src: string
-  readonly alt: string
-  readonly height?: string
-}
-
-export type Carousel = {
-  readonly alt: string
-  readonly src: string
-  readonly key: number
-}

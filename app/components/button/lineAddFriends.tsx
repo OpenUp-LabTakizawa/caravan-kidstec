7c17152cec64aff69d1673dc83aafeaf9391de23
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-09-20 18:18:19 +0900

    🎨style: add scale down up animation to line button (#980)

--- app/components/button/lineAddFriends.tsx
+++ app/components/button/lineAddFriends.tsx
@@ -10,7 +10,7 @@ export function LineAddFriends({
 }: Readonly<{ linkLink: string }>): JSX.Element {
   return (
     <Bounce>
-      <Link href={linkLink} className="block mx-auto w-fit">
+      <Link href={linkLink} className="block mx-auto scale-down-up w-fit">
         <Image
           src="/line_add_friends.avif"
           width={116}
@@ -30,7 +30,7 @@ export function LineApply({
     <Bounce>
       <Link
         href={lineLink}
-        className={`bg-[#00C300] btn shadow-lg text-lg text-white ${styles.greenShine}`}
+        className={`bg-[#00C300] font-bold gap-2 h-12 inline-flex items-center justify-center px-4 rounded-xl scale-down-up shadow-lg text-lg text-white ${styles.greenShine}`}
       >
         LINEで簡単応募する！
         <ArrowRightIcon className={`size-5 ${styles.arrowRight}`} />

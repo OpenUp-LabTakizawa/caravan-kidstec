8081c554b1166d09d965a30b5afa755301365f5b
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-10-03 18:27:08 +0900

    ♻️refactor: remove divider classname (#1036)
    * ♻️refactor: remove divider classname
    
    * [autofix.ci] apply automated fixes
    
    * [autofix.ci] apply automated fixes (attempt 2/3)
    
    * [autofix.ci] apply automated fixes (attempt 3/3)
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- app/kanto_event/privacy-policy/divider.tsx
+++ app/kanto_event/privacy-policy/divider.tsx
@@ -3,7 +3,7 @@ import type { JSX } from "react"
 
 export function Divider(): JSX.Element {
   return (
-    <div className="divider">
+    <div className="flex flex-row gap-4 h-4 items-center my-4 self-stretch whitespace-nowrap after:bg-gray-300 after:content-[''] after:flex-grow after:h-0.5 after:w-full before:bg-gray-300 before:content-[''] before:flex-grow before:h-0.5 before:w-full">
       <HashtagIcon className="size-10" />
     </div>
   )

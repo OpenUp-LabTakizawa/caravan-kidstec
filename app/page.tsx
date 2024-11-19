379d1219c4ecf1e95ad3f2027662593692786567
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-11-19 16:37:09 +0900

    ✨feat: update kanto event

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,11 @@ import type { JSX } from "react"
 import { IndicatorCarousel, ReviewCarousel, TopCarousel } from "./carousel.tsx"
 import { Video } from "./components/media/video.tsx"
 import { Introduction } from "./introduction.tsx"
-import { HIROSHIMA, KANTO_EVENT, SECRETARIAT } from "./lib/constant.ts"
+import { HIROSHIMA, SECRETARIAT } from "./lib/constant.ts"
 
 export default function Home(): JSX.Element {
   return (
     <article className="pb-4 space-y-6 text-center">
-      {/* block className is necessary for Safari behavior */}
-      <Link
-        href={KANTO_EVENT.pathname}
-        className="block button-pop duration-200 ease-out sticky top-0 z-20 sm:inline sm:static"
-      >
-        <Image
-          src="/202410_kanto_banner_full.avif"
-          width={540}
-          height={162}
-          alt="こどもテックキャラバン-関東イベントバナー"
-          priority={true}
-          className="duration-200 ease-out w-full"
-        />
-      </Link>
       <section>
         <TopCarousel />
       </section>

e59d7e5df79ff84e0930d1d6276dc28cc4a57e18
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-11-25 13:59:55 +0900

    🆙bump: version up tailwindCSS v4 (#1127)
    * 🆙bump: version up tailwindCSS v4
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -46,7 +46,7 @@ export default function HistoryPage(): JSX.Element {
             <section key={history.date} className="space-y-4 px-2">
               <details className={`group w-full ${styles.details}`}>
                 <summary className="block bg-amber-50 font-bold font-zenMaruGothic p-4 relative rounded-2xl text-2xl group-open:rounded-b-none">
-                  <p className="absolute outline outline-offset-2 rounded text-rose-400 text-xs -rotate-45">
+                  <p className="absolute outline outline-offset-2 rounded-sm text-rose-400 text-xs -rotate-45">
                     大好評で
                     <br />
                     開催終了

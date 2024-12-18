98f239e36575c764b8951ccbc3fcebd2c5a91d5a
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-12-18 14:30:46 +0900

    🆙bump: update dependencies (#1160)
    * 🆙bump: update dependencies
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- app/@modal/(.)history/image/[area]/[date]/[image]/page.tsx
+++ app/@modal/(.)history/image/[area]/[date]/[image]/page.tsx
@@ -1,10 +1,41 @@
 import { Modal } from "@/app/@modal/(.)history/modal"
 import type { Picture } from "@/app/interfaces/picture"
 import type { EventDate } from "@/app/interfaces/schedule"
-import { HIROSHIMA_HISTORY, KANTO, KANTO_HISTORY } from "@/app/lib/constant"
+import {
+  HIROSHIMA,
+  HIROSHIMA_HISTORY,
+  KANTO,
+  KANTO_HISTORY,
+} from "@/app/lib/constant"
 import { cloudfrontLoader } from "@/app/lib/loader"
 import Image from "next/image"
 import type { JSX } from "react"
+
+export function generateStaticParams(): {
+  area: string
+  date: string
+  image: string
+}[] {
+  const kantoArea: string = KANTO.pathname.split("/")[1]
+  const kantoParams: { area: string; date: string; image: string }[] =
+    KANTO_HISTORY.flatMap((eventDate) =>
+      eventDate.pictures.map((picture) => ({
+        area: kantoArea,
+        date: eventDate.date,
+        image: picture.src.split("/").reverse()[0].split(".")[0],
+      })),
+    )
+  const hiroshimaArea: string = HIROSHIMA.pathname.split("/")[1]
+  const hiroshimaParams: { area: string; date: string; image: string }[] =
+    HIROSHIMA_HISTORY.flatMap((eventDate) =>
+      eventDate.pictures.map((picture) => ({
+        area: hiroshimaArea,
+        date: eventDate.date,
+        image: picture.src.split("/").reverse()[0].split(".")[0],
+      })),
+    )
+  return [...kantoParams, ...hiroshimaParams]
+}
 
 export default async function PictureModal({
   params,

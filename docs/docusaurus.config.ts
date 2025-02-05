00d23ada68a9728e1bd21e5ba4e5c0f8fc7561d8
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-02-05 16:28:43 +0900

    🚑fix: docusaurus editUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/OpenUp-LabTakizawa/caravan-kidstec/tree/main/docs/docs/",
+            "https://github.com/OpenUp-LabTakizawa/caravan-kidstec/tree/main/docs/",
           onInlineTags: "warn",
         },
         theme: {

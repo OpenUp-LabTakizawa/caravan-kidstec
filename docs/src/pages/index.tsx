f01fc9d633a1a29ffc061ba2f93b8ba122b52280
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-02-18 15:11:59 +0900

    📝docs: translate tutorial

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,9 +20,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/tutorial"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            チュートリアル - 5分 ⏱️
           </Link>
         </div>
       </div>

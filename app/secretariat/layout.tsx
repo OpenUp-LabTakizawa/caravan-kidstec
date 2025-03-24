229ff20c0a529c9abd4eb1b3899484d888305cfb
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-03-24 14:17:29 +0900

    test

--- app/secretariat/layout.tsx
+++ app/secretariat/layout.tsx
@@ -1,9 +1,37 @@
-import { SECRETARIAT, SITE_TITLE } from "@/app/lib/constant"
+import {
+  CLOUDFRONT_URL,
+  DESCRIPTION,
+  SECRETARIAT,
+  SITE_TITLE,
+} from "@/app/lib/constant.ts"
 import type { Metadata } from "next"
 import type { JSX, ReactNode } from "react"
 
 export const metadata: Metadata = {
   title: SITE_TITLE + SECRETARIAT.name,
+  openGraph: {
+    images: [
+      {
+        url: `${CLOUDFRONT_URL}/logo/202505_event_ready.webp`,
+        width: 1200,
+        height: 848,
+        alt: `${SITE_TITLE} - プログラミング×体験学習`,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: SITE_TITLE,
+    description: DESCRIPTION,
+    images: [
+      {
+        url: `${CLOUDFRONT_URL}/logo/202505_event_ready.webp`,
+        width: 1200,
+        height: 848,
+        alt: `${SITE_TITLE} - プログラミング×体験学習`,
+      },
+    ],
+  },
 }
 
 export default function SecretariatLayout({

33c7a5fd4ada07440171fcb0b0992955dbcdc2a5
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-03-25 09:43:54 +0900

    🚑fix: change schedule font style

--- app/kanto_event/schedules.tsx
+++ app/kanto_event/schedules.tsx
@@ -15,32 +15,27 @@ export function Schedules({
             key={schedule.venue}
             className={`p-2 space-y-2${index !== schedules.length - 1 ? " border-b border-gray-200" : ""}`}
           >
-            <p className="flex items-center">
-              <span className="bg-gray-100 border border-current px-1.5 rounded-3xl text-xs">
+            <p className="flex items-center min-h-8">
+              <span className="bg-gray-100 border px-1.5 rounded-3xl">
                 Day{index + 1}
               </span>
-              <span className="decoration-4 decoration-sky-400 grow underline">
-                {schedule.date.year}年
-                <b className="text-2xl">{schedule.date.month}</b>月
-                <b className="decoration-4 decoration-sky-400 text-2xl underline">
-                  {schedule.date.day}
-                </b>
-                日(
-                <b className="text-xl">{schedule.date.dayOfWeek}</b>)
+              <span className="grow">
+                {schedule.date.year}年{schedule.date.month}月{schedule.date.day}
+                日({schedule.date.dayOfWeek})
               </span>
             </p>
-            <p className="flex items-center">
-              <span className="bg-gray-100 border border-current px-1.5 rounded-3xl text-xs">
+            <p className="flex items-center min-h-8">
+              <span className="bg-gray-100 border border-current px-1.5 rounded-3xl">
                 開始
               </span>
-              <b className="grow">10：00&nbsp;～&nbsp;17：00（予定）</b>
+              <span className="grow">10：00&nbsp;～&nbsp;17：00（予定）</span>
             </p>
-            <div className="flex gap-1 items-center sm:gap-2">
-              <p className="bg-gray-100 border border-current px-1.5 rounded-3xl text-xs">
+            <div className="flex gap-1 items-center min-h-8 sm:gap-2">
+              <p className="bg-gray-100 border border-current px-1.5 rounded-3xl">
                 場所
               </p>
               <div className="grow">
-                <b>{schedule.venue}</b>
+                <span>{schedule.venue}</span>
                 <Link
                   href={schedule.googleMapLink}
                   target="_blank"

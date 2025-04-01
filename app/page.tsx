31c8a91c95c718c6ce584995ebb83f4363aa079a
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-04-01 17:26:30 +0900

    🚀feat: add news list

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { PlusIcon } from "@heroicons/react/24/outline"
+import { ChevronRightIcon, PlusIcon } from "@heroicons/react/24/outline"
 import { Link } from "next-view-transitions"
 import Image from "next/image"
 import type { JSX } from "react"
 import { IndicatorCarousel, ReviewCarousel } from "./carousel.tsx"
 import { Video } from "./components/media/video.tsx"
 import { Introduction } from "./introduction.tsx"
-import { KANTO, KANTO_EVENT, SECRETARIAT } from "./lib/constant.ts"
+import { KANTO, KANTO_EVENT, NEWS, SECRETARIAT } from "./lib/constant.ts"
 
 export default function Home(): JSX.Element {
   return (
@@ -24,8 +24,58 @@ export default function Home(): JSX.Element {
         />
       </Link>
       <Video date="202410" pathname={KANTO.pathname} />
+      <section className="mx-2 space-y-4">
+        <h2 className="font-bold text-3xl">ニュース</h2>
+        <ul className="border-2 border-double border-orange-400 p-2 rounded-2xl space-y-2 text-center">
+          {NEWS.map((news) => (
+            <li
+              key={news.title}
+              className="bg-orange-100 border-b border-gray-300 duration-300 hover:bg-orange-200 hover:translate-x-2"
+            >
+              <Link
+                href={news.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block border-l-6 border-orange-400 button-pop grow"
+              >
+                <strong className="max-w-full whitespace-pre sm:whitespace-normal">
+                  {news.title}
+                </strong>
+                <p className="pr-2 text-gray-500 text-right text-xs sm:text-sm md:text-base">
+                  {news.date}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
       <section className="font-bold leading-7 space-y-4 text-base text-center tracking-[.29em]">
         <Introduction />
+      </section>
+      <section className="mx-2 space-y-4">
+        <h2 className="font-bold text-3xl">ニュース</h2>
+        <ul className="border-2 border-double border-orange-400 p-2 rounded-2xl space-y-2 text-center">
+          {NEWS.map((news) => (
+            <li
+              key={news.title}
+              className="bg-orange-100 border-b border-gray-300 duration-300 hover:bg-orange-200 hover:translate-x-2"
+            >
+              <Link
+                href={news.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block border-l-6 border-orange-400 button-pop grow"
+              >
+                <strong className="max-w-full whitespace-pre sm:whitespace-normal">
+                  {news.title}
+                </strong>
+                <p className="pr-2 text-gray-500 text-right text-xs sm:text-sm md:text-base">
+                  {news.date}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </section>
       <section className="bg-sky-300 mx-2 pb-2 rounded-2xl space-y-2">
         <div className="font-bold p-4 space-y-2 text-base text-white">
@@ -46,6 +96,32 @@ export default function Home(): JSX.Element {
           <p className="mt-2">過去の体験学習はこちら！</p>
         </div>
         <IndicatorCarousel />
+      </section>
+      <section className="mx-2 space-y-4">
+        <h2 className="font-bold text-3xl">ニュース</h2>
+        <ul className="border-2 border-dashed border-sky-400 p-2 rounded-2xl space-y-2 text-center">
+          {NEWS.map((news) => (
+            <li
+              key={news.title}
+              className="bg-sky-100 border-b border-gray-300 duration-300 flex items-center rounded-lg hover:bg-sky-200 hover:scale-105"
+            >
+              <ChevronRightIcon className="size-6 stroke-4 text-orange-400" />
+              <Link
+                href={news.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block button-pop grow"
+              >
+                <strong className="max-w-full whitespace-pre sm:whitespace-normal">
+                  {news.title}
+                </strong>
+                <p className="pr-2 text-gray-500 text-right text-xs sm:text-sm md:text-base">
+                  {news.date}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </section>
       <section className="space-y-4">
         <h2 className="font-bold text-3xl">参加者の声</h2>

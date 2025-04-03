f6ff4001f516d4248e5ed355d80d01155b07d302
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-04-03 15:03:07 +0900

    🚀feat: test line login

---
+++ auth.ts
@@ -0,0 +1,7 @@
+import NextAuth from "next-auth"
+import LINE from "next-auth/providers/line"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
+  providers: [LINE],
+})

34b98842e8206ce2d039e8d377e47409243eaae7
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-04-21 13:33:09 +0900

    ♻️refactor: remove unnecessary line value

--- auth.ts
+++ auth.ts
@@ -2,9 +2,5 @@ import NextAuth from "next-auth"
 import LINE from "next-auth/providers/line"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    LINE({
-      checks: ["state"],
-    }),
-  ],
+  providers: [LINE],
 })

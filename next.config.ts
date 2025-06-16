f8ad653423c6e8b0ff2092139a1a2f219c8b3c58
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-06-16 14:39:14 +0900

    fix: 🚑 show top page fadein animation (#1439)

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next"
+import withRspack from "next-rspack"
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -15,4 +16,4 @@ const nextConfig: NextConfig = {
   output: "standalone",
 }
 
-export default nextConfig
+export default withRspack(nextConfig)

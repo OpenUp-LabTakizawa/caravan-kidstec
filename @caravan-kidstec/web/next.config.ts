2a2f461fb37bc858b1d56dac5a9a44ac3fbf8fe1
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-08-28 18:46:13 +0900

    chore: remove unused images

--- @caravan-kidstec/web/next.config.ts
+++ @caravan-kidstec/web/next.config.ts
@@ -10,6 +10,24 @@ const nextConfig: NextConfig = {
   },
   images: {
     formats: ["image/avif", "image/webp"],
+    localPatterns: [
+      {
+        pathname: "/caravan-kidstec_logo_line.avif",
+        search: "",
+      },
+      {
+        pathname: "/handshake.avif",
+        search: "",
+      },
+      {
+        pathname: "/Instagram_Glyph_Gradient.avif",
+        search: "",
+      },
+      {
+        pathname: "/line_add_friends.avif",
+        search: "",
+      },
+    ],
     remotePatterns: [new URL("https://dk75m1tgsot44.cloudfront.net/**")],
   },
   output: "standalone",
